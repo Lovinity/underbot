228c9474f9531d5aab9b37ece5a7a4954a8aa4dc
Patrick Schmalstig <xanaftp@gmail.com>
2020-06-08 14:30:58 -0400

    ...

--- api/helpers/commands/rp-to-text.js
+++ api/helpers/commands/rp-to-text.js
@@ -30,7 +30,7 @@ module.exports = {
             throw new Error(`You are not allowed to use this command.`);
         }
 
-        var message = await inputs.message.send(`:hourglass: Please wait; this could take a while... (generating list of channels to fetch messages)`);
+        var mg = await inputs.message.send(`:hourglass: Please wait; this could take a while... (generating list of channels to fetch messages)`);
 
         var categories = [
             '710408355373383772', // ruins
@@ -74,7 +74,7 @@ module.exports = {
 
             sails.log.debug(`nextChannel channel name ${channels[ index ].name}`)
 
-            await message.edit(`:hourglass: Please wait; this could take a while... (Fetching messages from channel ${index + 1}/${channels.length})`);
+            await mg.edit(`:hourglass: Please wait; this could take a while... (Fetching messages from channel ${index + 1}/${channels.length})`);
 
             currentSnowflake = afterSnowflake;
 
@@ -108,33 +108,50 @@ module.exports = {
 
         sails.log.debug(`nextChannel DONE`)
 
-        await message.edit(`:hourglass: Please wait; this could take a while... (Sorting messages)`);
+        await mg.edit(`:hourglass: Please wait; this could take a while... (Sorting messages)`);
 
         messages = messages.sort(compare);
 
-        await message.edit(`:hourglass: Please wait; this could take a while... (Generating text file)`);
+        await mg.edit(`:hourglass: Please wait; this could take a while... (Generating text attachments)`);
 
-        var data = ``;
+        var splitMessages = (() => {
+            return new Promise(async (resolve, reject) => {
+                setTimeout(async () => {
+                    var msgs = messages.splice(0, 500);
 
-        messages.map((message) => {
-            // Write each message to data
-            data += `+++Message by ${message.author.username}#${message.author.discriminator} (${message.author.id}), ID ${message.id}, channel ${message.channel.name}+++\n`;
-            data += `-Time: ${moment(message.createdAt).format()}\n`;
-            // Write attachment URLs
-            message.attachments.array().map((attachment) => {
-                data += `-Attachment: ${attachment.url}\n`;
+                    var data = ``;
+
+                    msgs.map((message) => {
+                        // Write each message to data
+                        data += `+++Message in channel ${message.channel.parent ? message.channel.parent.name : `Unknown`} => ${message.channel.name} by ${message.author.username}#${message.author.discriminator} (${message.author.id}), ID ${message.id}+++\n`;
+                        data += `-Time: ${moment(message.createdAt).format()}\n`;
+                        // Write attachment URLs
+                        message.attachments.array().map((attachment) => {
+                            data += `-Attachment: ${attachment.url}\n`;
+                        });
+                        // Write embeds as JSON
+                        message.embeds.forEach((embed) => {
+                            data += `-Embed: ${JSON.stringify(embed)}\n`;
+                        });
+                        // Write the clean version of the message content
+                        data += `${message.cleanContent}\n\n\n`;
+                    });
+
+                    var buffer = Buffer.from(data, "utf-8");
+
+                    await inputs.message.send({ files: [ { attachment: buffer, name: `RPtext_${await sails.helpers.uid()}.txt` } ] });
+
+                    if (messages.length > 0)
+                        await splitMessages();
+
+                    return resolve();
+                }, 3000)
             });
-            // Write embeds as JSON
-            message.embeds.forEach((embed) => {
-                data += `-Embed: ${JSON.stringify(embed)}\n`;
-            });
-            // Write the clean version of the message content
-            data += `${message.cleanContent}\n\n\n`;
         });
 
-        var buffer = Buffer.from(data, "utf-8");
+        await splitMessages();
 
-        await message.edit(`DONE!`, { files: [ { attachment: buffer, name: `RPtext.txt` } ] });
+        await mg.edit(`DONE!`);
     }
 
 
