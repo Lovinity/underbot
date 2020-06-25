2728a14ce2fb27c253d7ebfde4de2841d1035ff3
Patrick Schmalstig <xanaftp@gmail.com>
2020-06-25 01:44:40 -0400

    Try self-executing functions for schedule

--- api/helpers/schedules/add.js
+++ api/helpers/schedules/add.js
@@ -29,40 +29,44 @@ module.exports = {
 
     // Tasks that have a one-time run
     if (inputs.record.nextRun && !inputs.record.cron) {
-      Schedules[ inputs.record.id ] = new CronJob(moment(inputs.record.nextRun).toDate(), async () => {
-        await sails.helpers.tasks[ inputs.record.task ].with(inputs.record.data || {});
-
-        // Destroy the one-time schedule
-        await sails.models.schedules.destroy({ id: inputs.record.id }).fetch();
-      }, null, true, "UTC");
+      var temp = (async (record) => {
+        Schedules[ record.id ] = new CronJob(moment(record.nextRun).toDate(), async () => {
+          await sails.helpers.tasks[ record.task ].with(record.data || {});
+  
+          // Destroy the one-time schedule
+          await sails.models.schedules.destroy({ id: record.id }).fetch();
+        }, null, true, "UTC");
+      })(inputs.record);
 
       // Tasks that have a cron recurrence
     } else if (inputs.record.cron) {
       // If nextRun is specified, schedule a cron to schedule the cron at nextRun.
       if (inputs.record.nextRun && moment().isBefore(moment(inputs.record.nextRun))) {
         // Cron specific to nextRun, which when executed, schedules/starts the actual cron.
-        Schedules[ inputs.record.id ] = new CronJob(moment(inputs.record.nextRun).toDate(), async () => {
-          // schedule/start the actual cron now; make it fire once immediately.
-          Schedules[ inputs.record.id ].stop();
-          Schedules[ inputs.record.id ] = new CronJob(inputs.record.cron, async () => {
-            await sails.helpers.tasks[ inputs.record.task ].with(inputs.record.data || {});
+        var temp = (async (record) => {
+          Schedules[ record.id ] = new CronJob(moment(record.nextRun).toDate(), async () => {
+            // schedule/start the actual cron now; make it fire once immediately.
+            Schedules[ record.id ].stop();
+            Schedules[ record.id ] = new CronJob(record.cron, async () => {
+              await sails.helpers.tasks[ record.task ].with(record.data || {});
+      
+              // Update lastRun
+              await sails.models.schedules.updateOne({ id: record.id }, { lastRun: moment().toISOString(true) });
+            }, null, true, "UTC", null, true);
+          }, null, true, "UTC");
+        })(inputs.record);
+      } else { // no nextRun? Just schedule the cron
+        var temp = (async (record) => {
+          Schedules[ record.id ] = new CronJob(record.cron, async () => {
+            console.log('CRON tick');
+            await sails.helpers.tasks[ record.task ].with(record.data || {});
     
             // Update lastRun
-            await sails.models.schedules.update({ id: inputs.record.id }, { lastRun: moment().toISOString(true) }).fetch();
-          }, null, true, "UTC", null, true);
-        }, null, true, "UTC");
-      } else { // no nextRun? Just schedule the cron
-        Schedules[ inputs.record.id ] = new CronJob(inputs.record.cron, async () => {
-          await sails.helpers.tasks[ inputs.record.task ].with(inputs.record.data || {});
-  
-          // Update lastRun
-          await sails.models.schedules.update({ id: inputs.record.id }, { lastRun: moment().toISOString(true) }).fetch();
-        }, null, true, "UTC");
+            await sails.models.schedules.updateOne({ id: record.id }, { lastRun: moment().toISOString(true) });
+          }, null, true, "UTC");
+        })(inputs.record);
       }
     }
-
-    console.log('Schedule registered for ' + inputs.record.id);
-    console.dir(Schedules[ inputs.record.id ].nextDates());
   }
 
 
