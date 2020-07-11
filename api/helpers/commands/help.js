b4c42d60e384ffb91480dbab3d01b689c9229abc
Patrick Schmalstig <xanaftp@gmail.com>
2020-07-10 21:41:13 -0400

    Use different discordMenu; help command fix

--- api/helpers/commands/help.js
+++ api/helpers/commands/help.js
@@ -19,20 +19,28 @@ module.exports = {
   exits: {},
 
   fn: async function (inputs) {
+    var guildSettings = await inputs.message.guild.settings();
     var prefix =
-      inputs.message.guild.settings.prefix ||
-      sails.config.custom.discord.defaultPrefix;
-    var children = [];
+      guildSettings.prefix || sails.config.custom.discord.defaultPrefix;
 
     // Get info about a specific command
     var commandHelp = async (command) => {
       var info = sails.helpers.commands[command].toJSON();
       var embed = new Discord.MessageEmbed()
+        .setAuthor(
+          `Drago's Moderation - Help`,
+          `${Client.user.displayAvatarURL()}`
+        )
         .setTitle(`Bot Command ${prefix}${command}`)
         .setDescription(info.description)
+        .setColor(`#8800FF`)
         .addField(
           `Parameters (separate each with a " | " or a double space)`,
           "."
+        )
+        .setFooter(
+          `Help was requested by ${inputs.message.author.username}`,
+          `${inputs.message.author.displayAvatarURL({ dynamic: "true" })}`
         );
 
       for (var input in info.inputs) {
@@ -40,8 +48,51 @@ module.exports = {
           input !== "message" &&
           Object.prototype.hasOwnProperty.call(info.inputs, input)
         ) {
-          var value = info.inputs[input].required ? `**REQUIRED**` + "\n" : ``;
+          var value = info.inputs[input].required ? `**REQUIRED**` + "\n" : `OPTIONAL` + "\n";
           value += `Type: ${info.inputs[input].type}` + "\n";
+          if (info.inputs[input].isInteger) value += `Integer.` + "\n";
+          if (info.inputs[input].isCreditCard)
+            value += `Credit Card Number.` + "\n";
+          if (info.inputs[input].isEmail) value += `Email Address.` + "\n";
+          if (info.inputs[input].isHexColor)
+            value += `Hexadecimal Color.` + "\n";
+          if (info.inputs[input].isIP) value += `IP address.` + "\n";
+          if (info.inputs[input].isURL) value += `URL.` + "\n";
+          if (info.inputs[input].isUUID) value += `UUID.` + "\n";
+          if (info.inputs[input].regex)
+            value += `Regex: ${info.inputs[input].regex}` + "\n";
+          if (typeof info.inputs[input].custom !== "undefined")
+            value += `Property also has custom validation.` + "\n";
+          if (typeof info.inputs[input].isBefore !== "undefined") {
+            value +=
+              `Before Date: ${moment(info.inputs[input].isBefore).format()}` +
+              "\n";
+          }
+          if (typeof info.inputs[input].isAfter !== "undefined") {
+            value +=
+              `After Date: ${moment(info.inputs[input].isAfter).format()}` +
+              "\n";
+          }
+          if (
+            typeof info.inputs[input].defaultsTo !== "undefined" ||
+            info.inputs[input].allowNull
+          )
+            value +=
+              `Default Value: ${
+                typeof info.inputs[input].defaultsTo !== "undefined"
+                  ? info.inputs[input].defaultsTo
+                  : `null`
+              }` + "\n";
+          if (typeof info.inputs[input].isIn !== "undefined")
+            value +=
+              `Must be one of the following: [${info.inputs[input].isIn.join(
+                ", "
+              )}]` + "\n";
+          if (typeof info.inputs[input].isNotIn !== "undefined")
+            value +=
+              `Must NOT be one of the following: [${info.inputs[
+                input
+              ].isNotIn.join(", ")}]` + "\n";
           if (typeof info.inputs[input].min !== "undefined")
             value += `Minimum: ${info.inputs[input].min}` + "\n";
           if (typeof info.inputs[input].max !== "undefined")
@@ -60,47 +111,74 @@ module.exports = {
         }
       }
 
-      return inputs.message.send({ embed: embed });
+      return inputs.message.channel.send({ embed: embed });
     };
 
     // No command specified? Get general info about all commands
     if (!inputs.command) {
-      // Generate the menu items / commands
+      let commandsMain = [];
+      let commands = [];
+      let _commands = [];
+      let commands2 = [];
+
+      // Get all the commands
       for (var command in sails.helpers.commands) {
         if (
           Object.prototype.hasOwnProperty.call(sails.helpers.commands, command)
         ) {
-          let tmp = ((cmd) => {
-            children.push({
-              title: `${prefix}${cmd}`,
-              description: sails.helpers.commands[cmd].toJSON().description,
-              function: (outputMessage, senderMessage) => {
-                outputMessage.delete();
-                return commandHelp(cmd);
-              },
-            });
-          })(command);
+          commands.push(command);
+          commandsMain.push(command);
         }
       }
 
-      // Construct the menu
-      var menuTemplate = {
-        title: `Available Bot Commands`,
-        description: `Here is a list of available commands in this bot. For more information about a specific command, choose its number in the menu.`,
-        color: "YELLOW",
-        children: children,
-      };
-      var response = await inputs.message.reply(`Please wait...`);
-      const menu = new Discord.DiscordMenu(
-        menuTemplate,
-        response,
-        inputs.message,
-        {
-          backButton: true,
-          time: 180,
+      // Now, break the commands up into groups of 10 for pagination.
+      while (commands.length > 0) {
+        _commands.push(commands.shift());
+        if (_commands.length > 9) {
+          commands2.push(_.cloneDeep(_commands));
+          _commands = [];
         }
+      }
+      if (_commands.length > 0) {
+        commands2.push(_.cloneDeep(_commands));
+      }
+
+      // Construct the Discord Menu
+      return new Discord.DiscordMenu(
+        inputs.message.channel,
+        inputs.message.author.id,
+        commands2.map((group) => {
+          var groupEmbed = new Discord.MessageEmbed()
+            .setAuthor(
+              `Drago's Moderation - Help`,
+              `${Client.user.displayAvatarURL()}`
+            )
+            .setDescription(
+              `Here is a list of available commands in the bot ( use the prefix **${prefix}** at the beginning of a command name to execute it ). Use the reactions to scroll between pages. Type a command name to view more info about that command.`
+            )
+            .setColor(`#8800FF`)
+            .setFooter(
+              `Help was requested by ${inputs.message.author.username}`,
+              `${inputs.message.author.displayAvatarURL({ dynamic: "true" })}`
+            );
+          group.map((cmd) => {
+            groupEmbed.addField(
+              cmd,
+              sails.helpers.commands[cmd].toJSON().description
+            );
+          });
+          return groupEmbed;
+        }),
+        commandsMain.map((cmd) => {
+          return {
+            message: cmd,
+            fn: (senderMessage) => {
+              senderMessage.delete();
+              return commandHelp(cmd);
+            },
+          };
+        })
       );
-      menu.start();
     } else {
       return commandHelp(inputs.command);
     }
--- util/DiscordMenu.js
+++ util/DiscordMenu.js
@@ -1,173 +1,88 @@
-let prevMenuCache;
-
-// Rich menu capabilities
-
-class DiscordMenu {
-  constructor(input, message, sender, opts) {
-    this.input = input;
-    this.message = message;
-    this.sender = sender;
-    for (let key in opts) this[key] = opts[key];
-  }
-
-  _handleError(code) {
-    let error = ``;
-    switch (code) {
-      case `MISSING_TITLE`:
-        error = `Menu should contain a title.`;
-        throw new Error(error);
-      case `MISSING_REACTIONS`:
-        error = `Reactions are needed in a menu.`;
-        throw new Error(error);
-      case `CHILDREN_NOT_ARRAY`:
-        error = `Children expected to be Array`;
-        throw new Error(error);
-      case `MISSING_FUNCTION`:
-        error = `Function property is missing or undefined.`;
-        throw new Error(error);
-      default:
-        error = `Unknown error occured.`;
-        throw new Error(error);
-    }
-  }
-
-  _handleFunction(func) {
-    return func(this.message, this.sender);
-  }
-
-  _handleEmbeds(menuData) {
-    //this.message.reactions.removeAll();
-    if (menuData.title === undefined) return this._handleError(`MISSING_TITLE`);
-    if (menuData.children === undefined) {
-      if (menuData.function === undefined)
-        return this._handleError(`MISSING_FUNCTION`);
-      else return this._handleFunction(menuData.function);
-    }
-
-    let reactions = {};
-    let _reactions = [];
-    let richEmbed = new Discord.MessageEmbed();
-
-    richEmbed.setTitle(menuData.title);
-    richEmbed.setDescription(menuData.description || ``);
-    richEmbed.setThumbnail(
-      menuData.thumbnail || (this.dataPersistance && this.input.thumbnail) || ``
-    );
-    richEmbed.setColor(
-      menuData.color || (this.dataPersistance && this.input.color) || `GREY`
-    );
-    if (menuData.footer !== undefined) {
-      menuData.footer.type === `timestamp`
-        ? richEmbed.setTimestamp(menuData.footer.value || Date.now())
-        : ``;
-      menuData.footer.type === `footer`
-        ? richEmbed.setTimestamp(menuData.footer.value || Date.now())
-        : ``;
-    } else if (this.dataPersistance && this.input.footer !== undefined) {
-      this.input.footer.type === `timestamp`
-        ? richEmbed.setTimestamp(this.input.footer.value || Date.now())
-        : ``;
-      this.input.footer.type === `footer`
-        ? richEmbed.setTimestamp(this.input.footer.value || Date.now())
-        : ``;
-    }
-
-    if (!Array.isArray(menuData.children))
-      return this._handleError(`CHILDREN_NOT_ARRAY`);
-
-    for (let i = 0; i < menuData.children.length; i++) {
-      let child = menuData.children[i];
-      let emoji = child.emoji || `${Object.keys(reactions).length + 1}⃣`;
-      richEmbed.addField(
-        `${emoji}: ${child.title}` || this._handleError(`MISSING_TITLE`),
-        child.description || `.`
-      );
-      reactions[emoji] = child.title;
-      _reactions.push(emoji);
-    }
-
-    this.message.edit({ embed: richEmbed }).then((_) => {
-      const addReactions = (emojis) => {
-        if (!emojis.length)
-          return prevMenuCache !== undefined &&
-            menuData !== this.input &&
-            this.backButton
-            ? this.message.react(this.input.backEmoji || `↩`)
-            : ``;
-        this.message.react(emojis[0]).then((_) => {
-          emojis.shift();
-          return addReactions(emojis);
-        });
-      };
-
-      addReactions(_reactions);
-
-      const filter = (reaction, user) => {
-        return (
-          (reactions[reaction.emoji.name] !== undefined ||
-            reaction.emoji.name === (this.input.backEmoji || `↩`)) &&
-          user.id === this.sender.author.id
-        );
-      };
-
-      this.message
-        .awaitReactions(filter, {
-          time: (this.time || 15) * 1000,
-          max: 1,
-          errors: [`time`],
-        })
-        .then((collected) => {
-          this.message.reactions.removeAll().then((_) => {
-            let chosen = collected.first().emoji.name;
-            if (this.backButton && chosen === (this.input.backEmoji || `↩`))
-              return this._handleEmbeds(prevMenuCache);
-            prevMenuCache = menuData;
-            if (reactions[chosen] === undefined)
-              return this.message.edit(
-                this.feedbackMessages.unknownOptions ||
-                  `Unknown option selected. Menu has been closed.`,
-                { embed: {} }
-              );
-            let childIndex = menuData.children.findIndex(
-              (v) => v.title === reactions[chosen]
-            );
-            this.message.edit(
-              (this.feedbackMessages && this.feedbackMessages.loadingMessage) ||
-                `Loading...`,
-              { embed: {} }
-            );
-            return this._handleEmbeds(menuData.children[childIndex]);
-          });
-        })
-        .catch((collected) => {
-          this.message.reactions.removeAll();
-          if (collected instanceof Error) {
-            return this.message.edit(
-              `An error occurred while executing the menu. Please check your console for more information.`,
-              { embed: {} }
-            );
-          }
-          return this.message.edit(
-            (this.feedbackMessages && this.feedbackMessages.timeoutMessage) ||
-              `Menu timed out - No response received from user.`,
-            { embed: {} }
-          );
-        });
+module.exports = class DiscordMenu {
+  constructor(
+    channel = new Discord.TextChannel(),
+    uid,
+    pages = [],
+    messages = [],
+    time = 180000,
+    reactions = { first: "⏪", back: "◀", next: "▶", last: "⏩", stop: "⏹" }
+  ) {
+    this.channel = channel;
+    this.pages = pages;
+    this.time = time;
+    this.reactions = reactions;
+    this.page = 1;
+    this.messages = messages;
+    channel.send(pages[0]).then((msg) => {
+      this.msg = msg;
+      this.addReactions();
+      this.createReactionCollector(uid);
+      this.createMessageCollector(uid);
     });
   }
-
-  start() {
-    prevMenuCache = undefined;
-    return this._handleEmbeds(this.input);
+  select(pg = 1) {
+    this.page = pg;
+    this.msg.edit(this.pages[pg - 1]);
   }
-
-  exit() {
-    this.message.reactions.removeAll();
-    return this.message.edit(
-      this.feedbackMessages.forcedExit || `Menu has been closed.`,
-      { embed: {} }
+  createReactionCollector(uid) {
+    let reactionCollector = this.msg.createReactionCollector(
+      (r, u) => u.id == uid,
+      { time: this.time }
     );
+    this.reactionCollector = reactionCollector;
+    reactionCollector.on("collect", (r) => {
+      if (r.emoji.name == this.reactions.first) {
+        if (this.page != 1) this.select(1);
+      } else if (r.emoji.name == this.reactions.back) {
+        if (this.page != 1) this.select(this.page - 1);
+      } else if (r.emoji.name == this.reactions.next) {
+        if (this.page != this.pages.length) this.select(this.page + 1);
+      } else if (r.emoji.name == this.reactions.last) {
+        if (this.page != this.pages.length) this.select(this.pages.length);
+      } else if (r.emoji.name == this.reactions.stop) {
+        reactionCollector.stop();
+      }
+      r.users.remove(uid);
+    });
+    reactionCollector.on("end", () => {
+      this.endCollection();
+    });
   }
-}
-
-module.exports = DiscordMenu;
+  createMessageCollector(uid) {
+    let messageCollector = this.channel.createMessageCollector(
+      (m) =>
+        m.author.id === uid &&
+        this.messages.find(
+          (m2) => m2.message.toLowerCase() === m.cleanContent.toLowerCase()
+        ),
+      { time: this.time }
+    );
+    this.messageCollector = messageCollector;
+    messageCollector.on("collect", (m) => {
+      this.endCollection();
+      let msgTrigger = this.messages.find(
+        (m2) => m2.message.toLowerCase() === m.cleanContent.toLowerCase()
+      );
+      if (msgTrigger && msgTrigger.fn) {
+        msgTrigger.fn(m);
+      }
+    });
+    messageCollector.on("end", () => {
+      this.endCollection();
+    });
+  }
+  endCollection() {
+    this.msg.delete();
+    if (this.reactionCollector && !this.reactionCollector.ended)
+      this.reactionCollector.stop();
+    if (this.messageCollector && !this.messageCollector.ended)
+      this.messageCollector.stop();
+  }
+  async addReactions() {
+    if (this.reactions.first) await this.msg.react(this.reactions.first);
+    if (this.reactions.back) await this.msg.react(this.reactions.back);
+    if (this.reactions.next) await this.msg.react(this.reactions.next);
+    if (this.reactions.last) await this.msg.react(this.reactions.last);
+    if (this.reactions.stop) await this.msg.react(this.reactions.stop);
+  }
+};
