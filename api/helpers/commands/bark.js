92c48297aecb39e35d2e236d48e7c2d2d9044fb1
Patrick Schmalstig <xanaftp@gmail.com>
2020-07-25 14:54:36 -0400

    Make pant work

--- api/helpers/commands/bark.js
+++ api/helpers/commands/bark.js
@@ -18,16 +18,7 @@ module.exports = {
   },
 
   fn: async function (inputs) {
-    var barks = [
-      "BARK",
-      "BORK",
-      "ARF",
-      "WOOF",
-      "RUFF",
-      "YIP",
-      "YAP",
-      "YIF",
-    ];
+    var barks = ["BARK", "BORK", "ARF", "WOOF", "RUFF", "YIP", "YAP", "YIF"];
     var rareBarks = ["NYA", "*SNEEZE*", "*GROWL SNARL*"];
 
     var len = getRandomInt(15, 30);
@@ -38,21 +29,16 @@ module.exports = {
     var random = getRandomInt(1, 3);
     var random2 = getRandomInt(1, 9);
     var random3 = getRandomInt(1, 50);
+    var spaced = true;
     for (var i = 0; i < len; i++) {
-      if (random3 !== 25) {
-        str += barks[barkIndex];
-        if (random2 === 8 || random2 === 9) {
-          str += `... `;
-          if (random === 1) barkIndex = getRandomInt(0, barks.length - 1);
-          random = getRandomInt(1, 3);
-        } else if (random2 !== 7 && random2 !== 6) {
-          str += ` `;
-          if (random === 1) barkIndex = getRandomInt(0, barks.length - 1);
-          random = getRandomInt(1, 3);
-        }
+      random2 = getRandomInt(1, 9);
+      random3 = getRandomInt(1, 50);
+
+      if (random3 === 25 && spaced) {
+        str += rareBarks[getRandomInt(0, rareBarks.length - 1)] + `... `;
+        spaced = true;
       } else if (
-        random2 !== 7 &&
-        random2 !== 6 &&
+        spaced &&
         (random3 === 30 ||
           random3 === 31 ||
           random3 === 32 ||
@@ -64,11 +50,25 @@ module.exports = {
           str += `*PANT* `;
         }
         str += `... `;
+        spaced = true;
       } else {
-        str += rareBarks[getRandomInt(0, rareBarks.length - 1)] + `... `;
+        str += barks[barkIndex];
+        if (random2 === 8 || random2 === 9) {
+          str += `... `;
+          spaced = true;
+          if (random === 1) barkIndex = getRandomInt(0, barks.length - 1);
+          random = getRandomInt(1, 3);
+        } else if (random2 !== 7 && random2 !== 6) {
+          str += ` `;
+          spaced = true;
+          if (random === 1) barkIndex = getRandomInt(0, barks.length - 1);
+          random = getRandomInt(1, 3);
+        } else {
+          spaced = false;
+          if (random === 1) barkIndex = getRandomInt(0, barks.length - 1);
+          random = getRandomInt(1, 3);
+        }
       }
-      random2 = getRandomInt(1, 9);
-      random3 = getRandomInt(1, 50);
     }
 
     return inputs.message.channel.send(str);
